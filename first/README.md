74a4be16c9b10f9082385caac0535b43aa2fcbd3
Altie Rose <altielynn@gmail.com>
2024-07-06 16:38:33 -0600

    init
    make flex vs grid example/practice

---
+++ first/README.md
@@ -0,0 +1,11 @@
+# an example of flex and grid made using react, tailwindcss, and vite
+This is a simple example to show how flex and grid work, and are diffrent for each other.
+
+This can be viewed at {ADD LINK HERE} but is better when you can edit the code.
+## commands
+| command | description |
+| --- | --- |
+| `pnpm install` | install dependencies |
+| `pnpm dev` | start the dev server |
+| `pnpm build` | build the app for production |
+| `pnpm preview` | preview the built app in the browser |
---
+++ first/src/App.tsx
@@ -0,0 +1,71 @@
+import { useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="min-h-screen bg-gray-700 text-gray-300 p-5">
+      <div className="flex justify-center gap-3 flex-col items-center">
+        <h1 className="text-5xl">Add item</h1>
+        <div className = "flex gap-3">
+          <button onClick={() => setCount(count + 1)} className='bg-green-500 rounded-lg p-2 text-white'>Add</button>
+          <button onClick={() => setCount(count + 10)} className='bg-green-500 rounded-lg p-2 text-white'>Add 10</button>
+          <button onClick={() => setCount(count + 100)} className='bg-green-500 rounded-lg p-2 text-white'>Add 100</button>
+          {
+            count <= 0 ? '' : <button onClick={() => setCount(count - 1)} className='bg-red-500 rounded-lg p-2 text-white'>Remove</button>
+          }
+          {
+            count <= 9 ? '' : <button onClick={() => setCount(count - 10)} className='bg-red-500 rounded-lg p-2 text-white'>Remove 10</button>
+          }
+          {
+            count <= 99 ? '' : <button onClick={() => setCount(count - 100)} className='bg-red-500 rounded-lg p-2 text-white'>Remove 100</button>
+          }
+          {
+            count <= 0 ? '' : <button onClick={() => setCount(count - count)} className='bg-orange-500 rounded-lg p-2 text-white'>Reset</button>
+          }
+        </div>
+        <h2 className="text-3xl">Count: {count}</h2>
+      </div>
+      {
+        count <= 0 ? '' : <h3 className="text-2xl">Flex</h3>
+      }
+      <div className="flex justify-center gap-3 flex-wrap flex-row">
+        {
+          Array(count).fill(0).map((_, i) => {
+            i = i + 1
+            if (i % 100 === 0) {
+              return <div key={i} className="bg-rose-500 rounded-lg flex-grow flex-shrink text-center min-w-16">{i}</div>
+            } else if (i % 10 === 0) {
+              return <div key={i} className="bg-yellow-500 rounded-lg flex-grow flex-shrink text-center min-w-16">{i}</div>
+            } else if (i % 2 === 0) {
+              return <div key={i} className="bg-pink-500 rounded-lg flex-grow flex-shrink text-center min-w-16">{i}</div>
+            } else {
+              return <div key={i} className="bg-blue-500 rounded-lg flex-grow flex-shrink text-center min-w-16">{i}</div>
+            }
+          })
+        }
+      </div>
+      {
+        count <= 0 ? '' : <h3 className="text-2xl">Grid</h3>
+      }
+      <div className="grid grid-cols-10 gap-3">
+        {
+          Array(count).fill(0).map((_, i) => {
+            i = i + 1
+            if (i % 100 === 0) {
+              return <div key={i} className="bg-rose-500 rounded-lg text-center min-w-16">{i}</div>
+            } else if ( i % 10 === 0) {
+              return <div key={i} className="bg-yellow-500 rounded-lg text-center min-w-16">{i}</div>
+            } else if (i % 2 === 0) {
+              return <div key={i} className="bg-pink-500 rounded-lg text-center min-w-16">{i}</div>
+            } else {
+              return <div key={i} className="bg-blue-500 rounded-lg text-center min-w-16">{i}</div>
+            }
+          })
+        }
+      </div>
+    </div>
+  )
+}
+
+export default App
